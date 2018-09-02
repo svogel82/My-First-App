39fc4fb2da5c2b88b2f67f3e41b39f4f481dd13c
Sebastian Vogel <info@sebastianvogel.com>
2018-09-02 12:06:55 +0200

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Sebastian Vogel on 02.09.18.
+//  Copyright © 2018 Sebastian Vogel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
